a467e85b6ffc86ed9322d4abff31ac23501a437c
RarceD <akimbo170@gmail.com>
2020-09-13 17:29:26 +0200

    conforming the idea

---
+++ Code/BEACON_SIMPLE_SCAN/BEACON_SIMPLE_SCAN.ino
@@ -0,0 +1,41 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+#include <BLEAddress.h>
+#include <BLEUUID.h>
+
+int scanTime = 2; //In seconds
+BLEScan* pBLEScan;
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+      //Serial.printf("Advertised Device: %s ", advertisedDevice.toString().c_str());
+      int rssi = advertisedDevice.getRSSI();
+      if (advertisedDevice.haveServiceUUID()){
+            Serial.printf("Name -> %s", advertisedDevice.getServiceUUID().toString().c_str());  
+      }
+      Serial.printf(", rssi -> %i \n", rssi);
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Scanning...");
+
+  BLEDevice::init("");
+  pBLEScan = BLEDevice::getScan(); //create new scan
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99);  // less or equal setInterval value
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+  Serial.print("Devices found: ");
+  Serial.print(foundDevices.getCount());
+  pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
+  delay(2000);
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# TFM--Bluetooth-Beacon-Tracking-
-Beacon Tracking 
+# Introduction #
+The main idea is to create a net of ESP32 boards  to track the nrf52832 slaves devises.  
+## ESP32 ##
+
+| Board Specifications  | Idea           
+| ---------------|:----------------:| 
+| Flash          |         m        | 
+| SRAM           |         m        | 
+
+
+## nrf52832 ##
+
+| Board Specifications  | Idea           
+| ---------------|:----------------:| 
+| Flash          |         m        | 
+| SRAM           |         m        | 
