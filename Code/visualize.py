e17f1d29f924a27e623747aeb207b2e4abdb35f1
RarceD <akimbo170@gmail.com>
2020-09-27 18:56:20 +0200

    beacon simulation done, TODO: correct saving process

--- Code/visualize.py
+++ Code/visualize.py
@@ -44,10 +44,10 @@ def connect_mqtt():
 
 def on_message(client, userdata, message):
     # print("message topic=",message.topic)
-    # print("message qos=",message.qos)
     # print("message retain flag=",message.retain)
+    # Example json: {"esp":"A1","beacon":[ {"uuid":5245,"distance":1.23},{"uuid":52345, "distance":1.23 }]}
     msg = str(message.payload.decode("utf-8"))
-    # print("message received: ", msg)
+    print("message received: ", msg)
     parsed_json = (json.loads(msg))
     global esp
     for e in esp:
@@ -60,19 +60,21 @@ def on_message(client, userdata, message):
                 beacon_uuid = str(parsed_json['beacon'][index]['uuid'])
                 # Add the beacon to the master if it is not repeat:
                 # print(index)
-                if (len(e.beacons) == 0):
-                    e.add_beacon(Beacon(0, 0, beacon_uuid, beacon_distance))
+                size_of_beacons = len(e.beacons)
+                if (size_of_beacons == 0):
+                    e.beacons.append(
+                        Beacon(0, 0, beacon_uuid, beacon_distance))
                     print('First beacon created: ', e.beacons[0].uuid)
                 else:
-                    size_of_beacons = len(e.beacons)
                     i = 0
                     while i < size_of_beacons:
+                        print('Element:', i, beacon_uuid, e.beacons[i].uuid)
                         if (beacon_uuid != str(e.beacons[i].uuid)):
-                            e.add_beacon(
+                            e.beacons.append(
                                 Beacon(0, 0, beacon_uuid, beacon_distance))
                             print('I create a beacon:', e.beacons[i].uuid)
                             break
-                        i+=1
+                        i += 1
         b = 0
         while (b < len(e.beacons)):
             print('Beacon nº', b, 'name:', e.beacons[b].uuid)
@@ -107,6 +109,7 @@ font = pygame.font.SysFont('bitstreamverasans', 30, True, True)
 
 
 def reddrawGameWindow():
+
     # always print first the background
     img_pos = 200
     win.fill((255, 255, 255))
@@ -122,12 +125,38 @@ def reddrawGameWindow():
             text_pos_y = img_pos + 120
         else:
             text_pos_y = img_pos - 40
-    for beacon in beacons:
-        beacon.draw(win)
+    # for beacon in beacons:
+    #     beacon.draw(win)
+    for e in esp:
+        for b in e.beacons:
+            b.draw(win)
     # pygame.draw.rect(win, (0, 0, 0), (550, 10, 120, 40))
     # pygame.draw.rect(win, (255, 0, 0), (555, 15, 110, 30))
     pygame.display.update()  # update the screen frames
 
+
+timer_update_screen = int(round(time.time()))
+refresh_time = 3
+reddrawGameWindow()
+# I create the beacons:
+
+# I fake create the masters with their beacons
+esp[0].beacons.append(
+    Beacon(random.randint(10, 699), 260, "E2:23:6A:54", 1.43))
+esp[0].beacons.append(
+    Beacon(random.randint(10, 699), 260, "V2:23:6A:54", 2.43))
+esp[1].beacons.append(
+    Beacon(random.randint(10, 699), 260, "V2:23:6A:54", 1.43))
+esp[1].beacons.append(
+    Beacon(random.randint(10, 699), 260, "32:34:5A:22", 2.43))
+esp[2].beacons.append(
+    Beacon(random.randint(10, 699), 260, "V3:23:6A:54", 1.43))
+esp[2].beacons.append(
+    Beacon(random.randint(10, 699), 260, "C2:34:5A:22", 2.43))
+for e in esp:
+    print('Master ESP:', e.uuid, 'has: ')
+    for b in e.beacons:
+        print('->', b.uuid, 'distance: ', b.distance)
 
 while(run):
     pygame.time.delay(50)  # 64x64 images
@@ -135,6 +164,31 @@ while(run):
         if event.type == pygame.QUIT:
             run = False
             client.loop_stop()  # stop the loop
+
     reddrawGameWindow()  # For drawing all the canvas
+    # Every 5 seconds I update the position of the screen:
+    if (int(round(time.time())) - timer_update_screen >= refresh_time):
+        # First I copy all the beacons in a local list:
+        is_in_esp = []
+        local_beacon = []
+        for e in esp:
+            for b in e.beacons:
+                if any(b.uuid in s for s in local_beacon):
+                    pass
+                else:
+                    local_beacon.append(b.uuid)
+        print('______The scan results______')
+        for b in local_beacon:
+            print(b)
+            is_in_esp.append('')
+        # Search for each beacon if is in more than one esp:
+        for b in local_beacon:
+            for e in esp:
+                for x in e.beacons:
+                    if (b == x.uuid):
+                        print('Is in ESP', e.uuid)
+                        # is_in_esp[]
+        timer_update_screen = int(round(time.time()))
+
 
 pygame.quit()
