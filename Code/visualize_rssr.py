c7c98afda3be5ab56039a2801c6c12d1dc7362ac
RarceD <akimbo170@gmail.com>
2020-10-10 10:00:13 +0200

    plot rssi in a table in order to filter

--- Code/visualize_rssr.py
+++ Code/visualize_rssr.py
@@ -3,16 +3,40 @@ import random
 import paho.mqtt.client as mqtt
 import time
 import json
+import numpy as np
 
 import pandas as pd  # module for
 from matplotlib import pyplot as plt
-plt.style.use('seaborn-dark')
 
 # I'm going to plot the results of rssi:
-x_rssi = []
-y_time = []
+rssi_b1 = []
+rssi_b2 = []
+x_time = []
 t_number = 0
 
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+
+
+
+
+# x = np.linspace(0, 10)
+
+# # Fixing random state for reproducibility
+# np.random.seed(19680801)
+
+# fig, ax = plt.subplots()
+
+# ax.plot(x, np.sin(x) + x + np.random.randn(50))
+# ax.plot(x, np.sin(x) + 0.5 * x + np.random.randn(50))
+# ax.plot(x, np.sin(x) + 2 * x + np.random.randn(50))
+# ax.plot(x, np.sin(x) - 0.5 * x + np.random.randn(50))
+# ax.plot(x, np.sin(x) - 2 * x + np.random.randn(50))
+# ax.plot(x, np.sin(x) + np.random.randn(50))
+# ax.set_title("'fivethirtyeight' style sheet")
+
+# plt.show()
 
 def connect_mqtt():
     broker_address = "broker.mqttdashboard.com"
@@ -30,7 +54,7 @@ def connect_mqtt():
 
 
 def on_message(client, userdata, message):
-    global x_rssi, y_time, t_number
+    global rssi_b1, rssi_b2, t_number, x_time
     # print("message topic=",message.topic)
     # print("message retain flag=",message.retain)
     # Example json: {"esp":"A1","beacon":[ {"uuid":5245,"distance":1.23},{"uuid":52345, "distance":1.23 }]}
@@ -41,40 +65,13 @@ def on_message(client, userdata, message):
         # Get the distance and the uuid of the beacon:
         beacon_distance = float(parsed_json['beacon'][index]['distance'])
         beacon_uuid = str(parsed_json['beacon'][index]['uuid'])
-        x_rssi.append(str(beacon_distance))
-        y_time.append(str(t_number))
 
-        # y_time.append(2)
+        ##I add the devices to a list:
+        rssi_b1.append(beacon_distance)
+        rssi_b2.append(beacon_distance+2.0)
+        x_time.append(str(t_number))
         t_number += 1
-        print(x_rssi, '34')
 
-
-def reddrawGameWindow():
-    """
-    # always print first the background
-    img_pos = 200
-    win.fill((255, 255, 255))
-    win.blit(bg, (0, 0))
-
-    render_text = ['A1', 'A2', 'A3']
-    text_pos_y = img_pos - 40
-    for index, r in enumerate(render_text):
-        x = font.render(r, 1, (0, 0, 0))
-        win.blit(x, (100 + 230*index, text_pos_y))
-        if index % 2 == 0:
-            text_pos_y = img_pos + 120
-        else:
-            text_pos_y = img_pos - 40
-    # for beacon in beacons:
-    #     beacon.draw(win)
-    for e in esp:
-        for b in e.beacons:
-            b.draw(win)
-    # pygame.draw.rect(win, (0, 0, 0), (550, 10, 120, 40))
-    # pygame.draw.rect(win, (255, 0, 0), (555, 15, 110, 30))
-    pygame.display.update()  # update the screen frames
-    """
-    pass
 
 def rssr_distance(rssi, tx, n):
     return 10**((tx-rssi)/n)
@@ -85,34 +82,23 @@ beacons = []
 esp = []
 position_adjustments = [0, 0]
 
-win_x = 700
-win_y = 500
 
 client = connect_mqtt()  # I connect to mqtt broker
-pygame.init()  # Inicialize pygame interface
-win = pygame.display.set_mode((win_x, win_y - 20))  # dimensions of it
-pygame.display.set_caption("Rolling tracker")  # title of this shit of game
-clock = pygame.time.Clock()
-bg = pygame.image.load('grass.jpg')
-font = pygame.font.SysFont('bitstreamverasans', 30, True, True)
+
 
 
 timer_update_screen = int(round(time.time()))
-refresh_time = 6
-reddrawGameWindow()
+refresh_time = 10
+# reddrawGameWindow()
 
 while(run):
-    pygame.time.delay(50)  # 64x64 images
-    for event in pygame.event.get():  # Check for events of close
-        if event.type == pygame.QUIT:
-            run = False
-            client.loop_stop()  # stop the loop
-            plt.plot(y_time, x_rssi,
-                     linestyle='--', marker='.', label='nop')
-            plt.show()
-
     # Every X seconds I update the position of the screen:
     if (int(round(time.time())) - timer_update_screen >= refresh_time):
-
+        run = False
+        client.loop_stop()  # stop the loop
+        ax.plot(x_time, rssi_b1, color='tab:blue')
+        ax.plot(x_time, rssi_b2, color='tab:orange')
+        ax.set_title("RSSI of beacons at 1 meter")
+        plt.show()
         timer_update_screen = int(round(time.time()))
-pygame.quit()
+
