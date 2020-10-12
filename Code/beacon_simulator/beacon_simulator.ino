9659f2645ded2c61b69a3fa02f9e372a2e6676b6
RarceD <akimbo170@gmail.com>
2020-10-12 10:40:58 +0200

    almost 100mA and 650ms on sending a beacon msg

--- Code/beacon_simulator/beacon_simulator.ino
+++ Code/beacon_simulator/beacon_simulator.ino
@@ -17,16 +17,16 @@
 #include "BLEBeacon.h"
 #include "esp_sleep.h"
 
-#define GPIO_DEEP_SLEEP_DURATION     10  // sleep x seconds and then wake up
+#define GPIO_DEEP_SLEEP_DURATION 5      // sleep x seconds and then wake up
 RTC_DATA_ATTR static time_t last;        // remember last boot in RTC Memory
 RTC_DATA_ATTR static uint32_t bootcount; // remember number of boots in RTC Memory
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-uint8_t temprature_sens_read();
-//uint8_t g_phyFuns;
+  uint8_t temprature_sens_read();
 
 #ifdef __cplusplus
 }
@@ -37,62 +37,57 @@ uint8_t temprature_sens_read();
 BLEAdvertising *pAdvertising;
 struct timeval now;
 
-#define BEACON_UUID "8ec76ea3-6668-48da-9866-75be8bc86f4d" 
-
-void setBeacon() {
-
+#define BEACON_UUID "8ec76ea3-eb0a-48da-9866-a0be0000a0be"
+#define LED_GREEN 15
+#define LED_RED 13
+#define LED_ORANGE_UP 12
+#define LED_ORANGE_DOWN 14
+void setBeacon()
+{
   BLEBeacon oBeacon = BLEBeacon();
   oBeacon.setManufacturerId(0x4C00); // fake Apple 0x004C LSB (ENDIAN_CHANGE_U16!)
   oBeacon.setProximityUUID(BLEUUID(BEACON_UUID));
-  oBeacon.setMajor((bootcount & 0xFFFF0000) >> 16);
-  oBeacon.setMinor(bootcount&0xFFFF);
+  // oBeacon.setMajor((bootcount & 0xFFFF0000) >> 16);
+  // oBeacon.setMinor(bootcount & 0xFFFF);
+  oBeacon.setMajor(0xF11FAAAA);
+  oBeacon.setMinor(0x11FF);
   BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
   BLEAdvertisementData oScanResponseData = BLEAdvertisementData();
-  
+
   oAdvertisementData.setFlags(0x04); // BR_EDR_NOT_SUPPORTED 0x04
-  
+
   std::string strServiceData = "";
-  
-  strServiceData += (char)26;     // Len
-  strServiceData += (char)0xFF;   // Type
-  strServiceData += oBeacon.getData(); 
+
+  strServiceData += (char)26;   // Len
+  strServiceData += (char)0xFF; // Type
+  strServiceData += oBeacon.getData();
   oAdvertisementData.addData(strServiceData);
-  
   pAdvertising->setAdvertisementData(oAdvertisementData);
   pAdvertising->setScanResponseData(oScanResponseData);
-
 }
 
-void setup() {
-
-    
+void setup()
+{
+    for (uint8_t i = 0; i<4;i++){
+  pinMode(12+i, OUTPUT);
+  }
+  digitalWrite(LED_RED, HIGH);
   Serial.begin(115200);
-  gettimeofday(&now, NULL);
-
-  Serial.printf("start ESP32 %d\n",bootcount++);
-
-  Serial.printf("deep sleep (%lds since last reset, %lds since last boot)\n",now.tv_sec,now.tv_sec-last);
-
-  last = now.tv_sec;
-  
+  Serial.println(millis());
   // Create the BLE Device
   BLEDevice::init("");
-
-  // Create the BLE Server
-  // BLEServer *pServer = BLEDevice::createServer(); // <-- no longer required to instantiate BLEServer, less flash and ram usage
-
   pAdvertising = BLEDevice::getAdvertising();
-  
   setBeacon();
-   // Start advertising
+  // Start advertising
   pAdvertising->start();
-  Serial.println("Advertizing started...");
-  delay(100);
+  delay(50);
   pAdvertising->stop();
-  Serial.printf("enter deep sleep\n");
+  Serial.printf("sleep: %i \n", millis());
+  digitalWrite(LED_RED, LOW);
+
   esp_deep_sleep(1000000LL * GPIO_DEEP_SLEEP_DURATION);
-  Serial.printf("in deep sleep\n");
 }
 
-void loop() {
+void loop()
+{
 }
