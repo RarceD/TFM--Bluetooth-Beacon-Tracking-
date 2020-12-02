7a22302469590ca7457d585afa75d50ab50a8f12
RarceD <akimbo170@gmail.com>
2020-12-02 22:22:54 +0100

    esp now send beacons data and sleep

--- Code/ESP_NOW/esp_node/esp_node.ino
+++ Code/ESP_NOW/esp_node/esp_node.ino
@@ -3,54 +3,50 @@
 
 // Structure example to receive data
 // Must match the sender structure
-typedef struct struct_message {
-    char a[32];
-    int b;
-    float c;
-    String d;
-    bool e;
-} struct_message;
+typedef struct Beacon_Information
+{
+  char uuid[20];
+  char beacon[20];
+  int rssi;
+} Beacon_Information;
 
 // Create a struct_message called myData
-struct_message myData;
+Beacon_Information myData;
 
 // callback function that will be executed when data is received
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
+{
   memcpy(&myData, incomingData, sizeof(myData));
   Serial.print("Bytes received: ");
   Serial.println(len);
-  Serial.print("Char: ");
-  Serial.println(myData.a);
-  Serial.print("Int: ");
-  Serial.println(myData.b);
-  Serial.print("Float: ");
-  Serial.println(myData.c);
-  Serial.print("String: ");
-  Serial.println(myData.d);
-  Serial.print("Bool: ");
-  Serial.println(myData.e);
+
+  Serial.print("UUID ");
+  for (uint8_t index = 0; index < 10; index++)
+    Serial.write(myData.uuid[index]);
   Serial.println();
 }
- 
-void setup() {
+
+void setup()
+{
   // Initialize Serial Monitor
   Serial.begin(115200);
-  
+
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
   // Init ESP-NOW
-  if (esp_now_init() != ESP_OK) {
+  if (esp_now_init() != ESP_OK)
+  {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-  
+
   // Once ESPNow is successfully Init, we will register for recv CB to
   // get recv packer info
   esp_now_register_recv_cb(OnDataRecv);
 }
- 
-void loop() {
-  esp_now_register_recv_cb(OnDataRecv);
 
+void loop()
+{
+  esp_now_register_recv_cb(OnDataRecv);
 }
