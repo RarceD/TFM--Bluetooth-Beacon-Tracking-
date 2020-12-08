c7082e88c23d4378543162677aff767e36e35029
RarceD <akimbo170@gmail.com>
2020-12-08 08:32:26 +0100

    locate 1 alone beacon and 2 trilateration

--- Code/Visualize_Tools/BeaconClass.py
+++ Code/Visualize_Tools/BeaconClass.py
@@ -41,5 +41,48 @@ class Esp:
         r = math.sqrt(ratio_linear)
         return r
 
+class Beacon_Data():
+    def __init__(self, name, rssi, esp):
+        self.name = name
+        self.esp = []
+        self.rssi = []
+        self.esp.append(esp)
+        self.rssi.append(rssi)
+        print("NEW Beacon: " + self.name)
 
+    def print_itself(self):
+        print(self.name)
+        for i in range(0, len(self.esp)):
+            print("-> ", self.esp[i], " - ", str(self.rssi[i]))
+        print("___________________________________________ ")
 
+    def add_rssi(self, esp, rssi):
+        self.esp.append(esp)
+        self.rssi.append(rssi)
+
+    def update_rssi(self, new_rssi, position):
+        self.rssi[position] = new_rssi
+
+def get_intersections(x0, y0, r0, x1, y1, r1):
+    # circle 1: (x0, y0), radius r0
+    # circle 2: (x1, y1), radius r1
+    d = math.sqrt((x1-x0)**2 + (y1-y0)**2)
+    # non intersecting
+    if d > r0 + r1:
+        return None
+    # One circle within other
+    if d < abs(r0-r1):
+        return None
+    # coincident circles
+    if d == 0 and r0 == r1: 
+        return None
+    else:
+        a = (r0**2-r1**2+d**2)/(2*d)
+        h = math.sqrt(r0**2-a**2)
+        x2 = x0+a*(x1-x0)/d
+        y2 = y0+a*(y1-y0)/d
+        x3 = x2+h*(y1-y0)/d
+        y3 = y2-h*(x1-x0)/d
+        x4 = x2-h*(y1-y0)/d
+        y4 = y2+h*(x1-x0)/d
+        return (x3, y3, x4, y4)
--- Code/Visualize_Tools/supermarket.py
+++ Code/Visualize_Tools/supermarket.py
@@ -5,63 +5,12 @@ import time
 import json
 import sys
 import math
-from BeaconClass import Beacon, Esp
-
-
-class Beacon_Data():
-    def __init__(self, name, rssi, esp):
-        self.name = name
-        self.esp = []
-        self.rssi = []
-        self.esp.append(esp)
-        self.rssi.append(rssi)
-        print("___________________________________________ ")
-        print("NEW Beacon: " + self.name)
-
-    def print_itself(self):
-        print(self.name)
-        for i in range(0, len(self.esp)):
-            print("-> ", self.esp[i], " - ", str(self.rssi[i]))
-        print("___________________________________________ ")
-
-    def add_rssi(self, esp, rssi):
-        self.esp.append(esp)
-        self.rssi.append(rssi)
-
-    def update_rssi(self, new_rssi, position):
-        self.rssi[position] = new_rssi
-
-    def get_intersections(self, x0, y0, r0, x1, y1, r1):
-        # circle 1: (x0, y0), radius r0
-        # circle 2: (x1, y1), radius r1
-        d = math.sqrt((x1-x0)**2 + (y1-y0)**2)
-        # non intersecting
-        if d > r0 + r1:
-            return None
-        # One circle within other
-        if d < abs(r0-r1):
-            return None
-        # coincident circles
-        if d == 0 and r0 == r1: 
-            return None
-        else:
-            a = (r0**2-r1**2+d**2)/(2*d)
-            h = math.sqrt(r0**2-a**2)
-            x2 = x0+a*(x1-x0)/d
-            y2 = y0+a*(y1-y0)/d
-            x3 = x2+h*(y1-y0)/d
-            y3 = y2-h*(x1-x0)/d
-
-            x4 = x2-h*(y1-y0)/d
-            y4 = y2+h*(x1-x0)/d
-
-            return (x3, y3, x4, y4)
+from BeaconClass import Beacon_Data, get_intersections
 
 
 PI = 3.141592653589793
 beacon_data = []
 beacon_data.append(Beacon_Data("test", 22, "esp_test"))
-
 
 def connect_mqtt():
     broker_address = "broker.mqttdashboard.com"
@@ -120,19 +69,51 @@ def on_message(client, userdata, message):
             a = Beacon_Data(beacon_uuid, beacon_distance, esp_local)
             beacon_data.append(a)
 
+def trilateration(beacon_data):
+    for b in beacon_data:
+        print(b.name)
+        if (len(b.esp) > 1):
+            # print("2 masters find the beacon")
+            x0 = 100
+            y0 = 50
+            r0 = abs(b.rssi[0]) * 6
+            x1 = 700
+            y1 = 550
+            r1 = abs(b.rssi[1]) * 6
+            pygame.draw.circle(win, (77, 126, 189), (x0, y0), int(r0),1)
+            pygame.draw.circle(win, (150, 74, 189), (x1, y1), int(r1),1)
+            points = get_intersections(x0, y0, r0, x1, y1, r1)
+            try:
+                intersection_points = []            
+                for p in points:
+                    intersection_points.append(int(p))
+                pygame.draw.circle(win, (234,random.randint(2,255), random.randint(2,255)), (intersection_points[0], intersection_points[1]), 7)
+                pygame.draw.circle(win, (234, random.randint(2,255), random.randint(2,255)), (intersection_points[2], intersection_points[3]), 7)
+            except:
+                pass
+
+            print(r0, r1, intersection_points)
+            
+
+
+        else:
+            pass
+            # print("1 master only")
 
 def reddrawGameWindow():
+    global beacon_data
     win.fill((145, 213, 250))
     win.blit(word_image, (0, 0))
+    
 
     # I set a grid to better positioning
     l_x, l_y = 0, 0
     debug = True
-    while (debug and (l_x <= win_x or l_y <= win_y)):
-        pygame.draw.line(win, (134, 136, 143), (l_x, 0), (l_x, win_y))
-        pygame.draw.line(win, (134, 136, 143), (0, l_y), (win_x, l_y))
-        l_y += 50
-        l_x += 50
+    # while (debug and (l_x <= win_x or l_y <= win_y)):
+    #     pygame.draw.line(win, (134, 136, 143), (l_x, 0), (l_x, win_y))
+    #     pygame.draw.line(win, (134, 136, 143), (0, l_y), (win_x, l_y))
+    #     l_y += 50
+    #     l_x += 50
 
     for e in esp:
         pygame.draw.rect(win, (0, 0, 0), (e.x, e.y, 25, 25))
@@ -143,40 +124,26 @@ def reddrawGameWindow():
     esp_center = [75, 25]
     r = 100
 
-    for i in range(0, 2):
-        pass
-    range_max = 7
-    for i in range(0, range_max):
-        rectangle = [-r + esp_center[0], -r+esp_center[1], 2*r, 2*r]
-        # pygame.draw.rect(win, (0, 0, 240), rectangle, 2)
-        pygame.draw.arc(win,  (64, 143, 222), rectangle, 3*PI/2, 0, 2)
-        r += 100
-    esp_center = [755, 575]
-    r = 100
-    for i in range(0, range_max):
-        rectangle = [-r + esp_center[0], -r+esp_center[1], 2*r, 2*r]
-        # pygame.draw.rect(win, (0, 0, 240), rectangle, 2)
-        pygame.draw.arc(win,  (217, 85, 41), rectangle, PI/2, PI, 2)
-        r += 100
-
-    surf1 = pygame.Surface((500, 500), pygame.SRCALPHA)
-    surf2 = pygame.Surface((500, 500), pygame.SRCALPHA)
-    pos1, rad1 = (250, 200), 100
-    pos2, rad2 = (250, 250), 80
-    pygame.draw.circle(surf1, (255, 0, 0, 255), pos1, rad1)
-    pygame.draw.circle(surf2, (255, 0, 0, 255), pos2, rad2)
-    surf1.blit(surf2, (0, 0), special_flags = pygame.BLEND_RGBA_MIN)
+    # for i in range(0, 2):
+    #     pass
+    # range_max = 7
+    # for i in range(0, range_max):
+    #     rectangle = [-r + esp_center[0], -r+esp_center[1], 2*r, 2*r]
+    #     # pygame.draw.rect(win, (0, 0, 240), rectangle, 2)
+    #     pygame.draw.arc(win,  (64, 143, 222), rectangle, 3*PI/2, 0, 2)
+    #     r += 100
+    # esp_center = [755, 575]
+    # r = 100
+    # for i in range(0, range_max):
+    #     rectangle = [-r + esp_center[0], -r+esp_center[1], 2*r, 2*r]
+    #     # pygame.draw.rect(win, (0, 0, 240), rectangle, 2)
+    #     pygame.draw.arc(win,  (217, 85, 41), rectangle, PI/2, PI, 2)
+    #     r += 100
+    trilateration(beacon_data)
+    
     pygame.display.update()  # update the screen frames
 
 
-def trilateration(beacon_data):
-    for b in beacon_data:
-        b.print_itself()
-        if (len(b.esp) > 1):
-            print("2 masters find the beacon")
-
-        else:
-            print("1 master only")
 
 
 
@@ -209,8 +176,7 @@ font = pygame.font.SysFont('bitstreamverasans', 30, True, True)
 # I also create global esps:
 beacons = []
 esp = []
-esp.append(Esp("A5", 75, 25))
-esp.append(Esp("A2", 725, 550))
+
 # Render for the first time the window:    w
 word_image = pygame.image.load('images/supermarket-floor.jpg')
 word_image = pygame.transform.scale(word_image, (win_x, win_y))
@@ -230,7 +196,7 @@ while(run):
     # Every X seconds I update the position of the screen:
     if (int(round(time.time())) - timer_update_screen >= refresh_time):
         # I calculate the positions:
-        trilateration(beacon_data)
+        # trilateration(beacon_data, win)
         # Render the screen:
         reddrawGameWindow()
         # Delete all the info:
